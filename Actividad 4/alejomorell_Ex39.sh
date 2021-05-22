499f9327d84148c2c6cc3a77dfb73277802b0bc3
Yukics <alejomorell@gmail.com>
2021-05-22 23:57:51 +0200

    39 remix v2

--- Actividad 4/alejomorell_Ex39.sh
+++ Actividad 4/alejomorell_Ex39.sh
@@ -24,7 +24,7 @@ do
     while read i; do #Leo cada linea del fichero anterior y busco coincidencias con el parametro especificado, en caso de que se cumpla la condición imprime el dispositivo, su utilización y punto de montaje
         if [ $i -ge $2 ]
         then
-            echo "`cat dev.usage | grep $(echo "$i%")`"
+            echo "Dispositivo: `cat dev.usage | grep $(echo "$i%") | awk '{print $1}'`, utilización: `cat dev.usage | grep $(echo "$i%") | awk '{print $5}'`, punto de montaje: `cat dev.usage | grep $(echo "$i%") | awk '{print $6}'` "
             exit
         fi        
     done < devper.usage  
