8a44dc83742f6e5ab407cbcdbeabffef13a8928a
Yukics <alejomorell@gmail.com>
2021-05-22 20:36:52 +0200

    33

--- Actividad 4/alejomorell_Ex33.sh
+++ Actividad 4/alejomorell_Ex33.sh
@@ -1,5 +1,18 @@
 #/bin/bash
+#Si se ejecuta con sh en vez de con bash, dará error
 #El tipo de usuario que debe ejecutar este script debe ser admin
-df | grep -E '((/dev/sd)|(/dev/nvme))' | sed 's/|/ /' | awk '{print $1}' > devices.mounted
-sudo parted /dev/sda
-lsblk | grep disk
+if [ $EUID == 0 ]
+then #Cuando se ejecuta como superusuario el user id es 0 de root, cuando se da la condicion imprime esto
+    nprim=`sudo parted -l 2> /dev/null | grep "primary" | wc -l` 
+    #Con esto sacamos los discos y sus particiones y en caso de error redirigimos la salidad a null, 
+    #y contamos cuantos de los discos son primarios, extendidos o logicos
+    echo "Nº de particiones primarias: $nprim"
+    next=`sudo parted -l 2> /dev/null | grep "extended" | wc -l`
+    echo "Nº de particiones extendidas: $next"
+    nlog=`sudo parted -l 2> /dev/null | grep "logical" | wc -l` 
+    #Debido a que logical es una palabra que tambien sale en la salida dando otro 
+    #tipo de información hay que restar uno al numero de logicals
+    echo "Nº de particiones logicas: $(($nlog - 1))"
+else
+    echo "Este script debe ejecutarse como root"
+fi
